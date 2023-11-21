f233d10b918901e4cf8c51e6852160c2ab8fdd75
Biscolad <biscolad@gmail.com>
2023-11-20 20:44:35 -0500

    POST-Create a new movie

--- index.mjs
+++ index.mjs
@@ -3,10 +3,16 @@ const app = express()
 const port = 4000
 
 import movies from "./routes/movies.mjs"
+import comments from "./routes/comments.mjs"
+import theaters from "./routes/theaters.mjs"
 
 //MIDDLEWARE
 app.use(express.json())
 app.use('/movies', movies)
+
+app.use('/comments', comments)
+
+app.use('/theaters', theaters)
 
 
 //ERROR HANDLING
--- routes/comments.mjs
+++ routes/comments.mjs
@@ -4,36 +4,22 @@ import {ObjectId} from 'mongodb'
 
 const router = express.Router()
 
-//GET - get all movies
+//GET - get all comments
 router.get("/", async(req, res) => {
-    const collection = await db.collection("movies")
+    const collection = await db.collection("comments")
     const results = await collection.find({}).limit(20).toArray()
     res.send(results).status(200)
 })
 
-//GET - show - get one movie
+//GET - show - get one comment
 router.get('/:id', async(req, res) => {
-    const collection = await db.collection('movies')
+    const collection = await db.collection('comments')
     const query = {_id: new ObjectId(req.params.id)}
     const result = await collection.findOne(query)
 
     if (!result) res.send("Not Found").status(404);
     else res.send(result).status(200)
 })
-
-
-// POST - create ne id for a movie //655c0a53549c4221d85ff7e5//
-router.post('/', async (req, res) => {
-    const collection = await db.collection('movies')
-    const newMovie = req.body; 
-    newMovie.date = new Date()
-    const result = await collection.insertOne(newMovie)
-    res.send(result).status(204)
-})
-
-
-
-
 
 
 
